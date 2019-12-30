67d170e50ab78ace3941f9a181245db61c43677c
nnandjj <54811765+nnandjj@users.noreply.github.com>
2019-12-31 00:32:13 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -33,7 +33,7 @@ $(document).ready(function() {
           $login.addClass("inactive");
           animating = false;
           $(that).removeClass("success processing");
-          window.location.replace("http://www.baidu.com/");
+          window.location.replace("https://nnandjj.github.io/jump/show.html");
         }, submitPhase2);
       }, submitPhase1);
     }
